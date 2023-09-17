ab77b5f695fa899f88a170491a662cabe5be97f7
Josh Mann <goostcreative@gmail.com>
2023-09-17 09:58:16 -0700

    Create Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,45 @@
-
 function Projects() {
-    return(
-        <>
-        
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <div className="container__mainImage"></div>
+        <div className="container__thumbnails">
+          <div className="container__thumbnail">
+            <div className="container__thumbnail--image"></div>
+            <div className="container__thumbnail--heading"></div>
+          </div>
+          <div className="container__thumbnail">
+            <div className="container__thumbnail--image"></div>
+            <div className="container__thumbnail--heading"></div>
+          </div>
+          <div className="container__thumbnail">
+            <div className="container__thumbnail--image"></div>
+            <div className="container__thumbnail--heading"></div>
+          </div>
+          <div className="container__thumbnail">
+            <div className="container__thumbnail--image"></div>
+            <div className="container__thumbnail--heading"></div>
+          </div>
+          <div className="container__thumbnail">
+            <div className="container__thumbnail--image"></div>
+            <div className="container__thumbnail--heading"></div>
+          </div>
+          <div className="container__thumbnail">
+            <div className="container__thumbnail--image"></div>
+            <div className="container__thumbnail--heading"></div>
+          </div>
+          <div className="container__thumbnail">
+            <div className="container__thumbnail--image"></div>
+            <div className="container__thumbnail--heading"></div>
+          </div>
+          <div className="container__thumbnail">
+            <div className="container__thumbnail--image"></div>
+            <div className="container__thumbnail--heading"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Projects;
